7a21abbcc0e0cd0d72a9c59aacd644d10f7a8c81
Ben Daniels <danielsb8@xavier.edu>
2026-03-30 20:17:17 -0400

    update for proper formatting

---
+++ java/main/Main.java
@@ -0,0 +1,5 @@
+//main for jokes
+
+public static void main(String[] args) {
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+// main for jokebot
+
+public class Main {
+    public static void main(String[] args) {
+        // Add code to print "Hello, world!" here
+        System.out.println("Hello, world!");
+    }
+}
